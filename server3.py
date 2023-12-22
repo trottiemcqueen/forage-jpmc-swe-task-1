fd4ece839204ba492f2320cbe29a106999f48211
Trottie McQueen <mcqueen.trottie007@gmail.com>
2023-12-22 00:10:49 -0500

    fixed broken issue

--- server3.py
+++ server3.py
@@ -39,7 +39,7 @@ import dateutil.parser
 
 # Sim params
 
-REALTIME = True
+REALTIME = True                                                                                                                                                                                                                                                               
 SIM_LENGTH = timedelta(days=365 * 5)
 MARKET_OPEN = datetime.today().replace(hour=0, minute=30, second=0)
 
